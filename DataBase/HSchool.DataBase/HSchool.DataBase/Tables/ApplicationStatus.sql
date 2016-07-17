e77c216451e8c988a3abbcf7c5d89152a37e151c
Pariventhan <pariventhan1984@gmail.com>
2016-07-17 15:24:38 +0530

    Included Projects

---
+++ DataBase/HSchool.DataBase/HSchool.DataBase/Tables/ApplicationStatus.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[ApplicationStatus]
+(
+	[ApplicationStatusId] INT NOT NULL PRIMARY KEY identity,
+	[ApplicationStatus] varchar(15)
+)
---
+++ DataBase/HSchool.DataBase/HSchool.DataBase/Tables/Applications.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Applications]
+(
+	[ApplicationId] INT NOT NULL PRIMARY KEY identity,
+	[UserId] int foreign key references dbo.UserAccounts (UserId),
+	[ApplicationStatus] int foreign key references dbo.ApplicationStatus (ApplicationStatusId),
+	[AppliedDate] datetime,
+	[ApprovedDate] datetime,
+	[ApplicationType] datetime,
+	[ApprovedBy] datetime,
+	[CreatedDate] datetime,
+	[ModifiedDate] datetime,
+	[IsDeleted] bit,
+)
